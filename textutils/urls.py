7670da9e3d99997fede83e825b85db561c3a82da
Tushar <tusharverma1510@gmail.com>
2021-06-11 16:34:16 +0530

    commit for text util

--- textutils/urls.py
+++ textutils/urls.py
@@ -1,7 +1,7 @@
-"""textutil URL Configuration
+"""textutils URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,14 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze'),
+    path('about', views.about, name='about')
 
-    path('',views.index,name='index'),
-
-    path('analzye',views.analzye,name='analzye'),
-
-    # path('removeDigits',views.removeDigits,name='home'),
-    
-    # path('removeSpace',views.delSpace,name='home'),
 ]
