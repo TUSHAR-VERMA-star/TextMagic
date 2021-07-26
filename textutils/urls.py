a0ba81d7c48bfa2c17881e3ae4c684c5fda6bbc5
Tushar <tusharverma1510@gmail.com>
2021-07-26 19:48:31 +0530

    push

--- textutils/urls.py
+++ textutils/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path , include
 from . import views
-
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('hello.urls'))
+    path('', include('hello.urls'))
 ]
