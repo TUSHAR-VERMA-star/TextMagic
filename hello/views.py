daf463197792cd8cc7540d22dbe9cf39b87b64d7
Tushar <tusharverma1510@gmail.com>
2021-07-26 23:16:49 +0530

    push

--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,19 @@
 # I have created this file - Harry
-from django.http import HttpResponse
 from django.shortcuts import render
+
 def index(request):
     return render(request, 'index.html')
 
 
 def analyze(request):
-    #Get the text
     djtext = request.POST.get('text', 'default')
-    #copy of text 
     copyOfText=djtext
-    # Check checkbox values
     removepunc = request.POST.get('removepunc', 'off')
     fullcaps = request.POST.get('fullcaps', 'off')
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
     numberremover = request.POST.get('numberremover','off')
     fulltextanalyze =request.POST.get('fulltextanalyze','off')
-    #Check which checkbox is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
@@ -39,7 +35,6 @@ def analyze(request):
     if(extraspaceremover=="on"):
         analyzed = ""
         for index, char in enumerate(djtext):
-            # It is for if a extraspace is in the last of the string
             if char == djtext[-1]:
                     if not(djtext[index] == " "):
                         analyzed = analyzed + char
