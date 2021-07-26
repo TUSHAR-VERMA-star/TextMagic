fe1b2d81a9192b04997ef315fd0b6b8165c37256
Tushar <tusharverma1510@gmail.com>
2021-07-26 19:51:42 +0530

    push

--- index.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path , include
-from . import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index,name='home'),
-    path('shop/',include('shop.urls')),
-    path('blog/',include('blog.urls'))
-]
